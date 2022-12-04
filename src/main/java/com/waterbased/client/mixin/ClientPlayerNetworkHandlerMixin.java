bda66dc2b7e0fb48e659b81b9938c91886cf3718
Finn Watermann <finn.watermann@arcor.de>
2022-12-04 03:53:05 +0100

    Make EntityGlow working

---
+++ src/main/java/com/waterbased/client/mixin/ClientPlayerNetworkHandlerMixin.java
@@ -0,0 +1,44 @@
+package com.waterbased.client.mixin;
+
+import com.waterbased.client.Client;
+import com.waterbased.client.modules.EntityGlow;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
+import net.minecraft.network.packet.s2c.play.EntityTrackerUpdateS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public class ClientPlayerNetworkHandlerMixin {
+
+
+    @Inject(at = @At("TAIL"), method = "onEntitySpawn", cancellable = true)
+    void onEntitySpawn(EntitySpawnS2CPacket packet, CallbackInfo ci) {
+
+        if(Client.INSTANCE.MODULE_MANAGER.getModule(EntityGlow.class).isEnabled()) {
+            Entity entity = MinecraftClient.getInstance().world.getEntityById(packet.getId());
+            Client.INSTANCE.MODULE_MANAGER.getModule(EntityGlow.class).onEntitySpawn(entity);
+        }
+
+    }
+
+    @Inject(at = @At("TAIL"), method = "onEntityTrackerUpdate", cancellable = true)
+    void onEntityStatusEffect(EntityTrackerUpdateS2CPacket packet, CallbackInfo ci) {
+
+        if(Client.INSTANCE.MODULE_MANAGER.getModule(EntityGlow.class).isEnabled()) {
+            Entity entity = MinecraftClient.getInstance().world.getEntityById(packet.id());
+            Client.INSTANCE.MODULE_MANAGER.getModule(EntityGlow.class).onEntityMetadataUpdate(entity);
+        }
+
+    }
+
+
+
+
+
+}
