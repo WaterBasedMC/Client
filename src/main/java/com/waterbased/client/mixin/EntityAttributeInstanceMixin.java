84477f918869dba32402af17591fe4e8b742decc
Flamtky <n.schumann@rz-online.de>
2022-12-04 03:48:41 +0100

    noslowdown works with statuseffect

---
+++ src/main/java/com/waterbased/client/mixin/EntityAttributeInstanceMixin.java
@@ -0,0 +1,25 @@
+package com.waterbased.client.mixin;
+
+import com.waterbased.client.Client;
+import com.waterbased.client.modules.NoSlowDown;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
+
+@Mixin(EntityAttributeInstance.class)
+public class EntityAttributeInstanceMixin {
+
+    @Inject(at = @At("HEAD"), method = "addModifier", cancellable = true)
+    private void addModifier(EntityAttributeModifier modifier, CallbackInfo ci) {
+        if (Client.INSTANCE.MODULE_MANAGER.getModule(NoSlowDown.class).isEnabled()) {
+            if (modifier.getId().equals(UUID.fromString(NoSlowDown.SLOWNESS_UUID))) {
+                ci.cancel();
+            }
+        }
+    }
+}
--- src/main/java/com/waterbased/client/modules/NoSlowDown.java
+++ src/main/java/com/waterbased/client/modules/NoSlowDown.java
@@ -5,6 +5,8 @@ import net.minecraft.client.util.InputUtil;
 
 public class NoSlowDown extends Module {
 
+    public static final String SLOWNESS_UUID = "7107DE5E-7CE8-4030-940E-514C1F160890";
+
     public NoSlowDown() {
         super("NoSlowDown", "Prevents you from slowing down, when using items or going through blocks", InputUtil.GLFW_KEY_B);
     }
