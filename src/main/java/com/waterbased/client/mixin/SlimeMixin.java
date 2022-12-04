fbc7e762f52ef0a09212e78f8554ec8fbed1d945
Flamtky <n.schumann@rz-online.de>
2022-12-04 03:09:31 +0100

    fixed noslowdown with slime

---
+++ src/main/java/com/waterbased/client/mixin/SlimeMixin.java
@@ -0,0 +1,24 @@
+package com.waterbased.client.mixin;
+
+import com.waterbased.client.Client;
+import com.waterbased.client.modules.NoSlowDown;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SlimeBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SlimeBlock.class)
+public class SlimeMixin {
+
+    @Inject(at = @At("HEAD"), method = "onSteppedOn", cancellable = true)
+    private void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity, CallbackInfo ci) {
+        if (Client.INSTANCE.MODULE_MANAGER.getModule(NoSlowDown.class).isEnabled()) {
+            ci.cancel();
+        }
+    }
+}
