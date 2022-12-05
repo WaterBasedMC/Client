54cf3f710f8969a32fe03bb21449005dc37d4088
Finn Watermann <finn.watermann@arcor.de>
2022-12-05 16:00:14 +0100

    Add BouncySlime module to click gui

--- src/main/java/com/waterbased/client/ui/clickgui/categories/CategoryMovement.java
+++ src/main/java/com/waterbased/client/ui/clickgui/categories/CategoryMovement.java
@@ -1,5 +1,6 @@
 package com.waterbased.client.ui.clickgui.categories;
 
+import com.waterbased.client.modules.movement.BouncySlime;
 import com.waterbased.client.modules.movement.Flight;
 import com.waterbased.client.modules.movement.NoSlowDown;
 import com.waterbased.client.modules.movement.SpectatorCam;
@@ -10,6 +11,7 @@ public class CategoryMovement extends CategoryDrawable {
     public CategoryMovement(int x, int y) {
         super("Movement", x, y);
 
+        this.addEntry(new CategoryModuleEntry(BouncySlime.class));
         this.addEntry(new CategoryModuleEntry(Flight.class));
         this.addEntry(new CategoryModuleEntry(NoSlowDown.class));
         this.addEntry(new CategoryModuleEntry(SpectatorCam.class));
