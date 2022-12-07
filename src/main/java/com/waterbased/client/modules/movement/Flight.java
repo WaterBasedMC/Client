b24bbf5d3d6114863e02182d6926a00973f9fb2a
Flamtky <n.schumann@rz-online.de>
2022-12-07 12:32:33 +0100

    removed nofall dep in flight

--- src/main/java/com/waterbased/client/modules/movement/Flight.java
+++ src/main/java/com/waterbased/client/modules/movement/Flight.java
@@ -1,8 +1,6 @@
 package com.waterbased.client.modules.movement;
 
-import com.waterbased.client.Client;
 import com.waterbased.client.modules.Module;
-import com.waterbased.client.modules.player.NoFall;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.util.InputUtil;
@@ -12,7 +10,6 @@ public class Flight extends Module {
     private float speed = 1f;
     private float speedMultiplier = 2f;
     private int ticks = 0;
-    private boolean oldNoFallState = false;
 
     public Flight() {
         super("Flight", "Allows you to fly in survival/adventure mode", InputUtil.GLFW_KEY_F);
@@ -21,15 +18,10 @@ public class Flight extends Module {
     @Override
     public void onEnable() {
         this.ticks = 0;
-        NoFall nf = Client.INSTANCE.MODULE_MANAGER.getModule(NoFall.class);
-        this.oldNoFallState = nf.isEnabled();
-        nf.forceState(true);
     }
 
     @Override
     public void onDisable() {
-        NoFall nf = Client.INSTANCE.MODULE_MANAGER.getModule(NoFall.class);
-        nf.forceState(this.oldNoFallState);
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
         if (player == null) return;
         player.airStrafingSpeed = 0.02f;
